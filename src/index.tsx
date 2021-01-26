0f8ffd362cfc5f6875875ee71045a4ba2df8a919
Cho Yeonwoo <wooisso@naver.com>
2021-01-26 11:57:26 +0900

    add webworker

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as serviceWorker from './service-worker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-serviceWorker.register()
+serviceWorkerRegistration.register()
