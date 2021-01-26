10ca68bb1f32a6ce088f44cdef7e908acfb37b75
Cho Yeonwoo <wooisso@naver.com>
2021-01-26 16:47:56 +0900

    delete unused import

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -13,7 +13,6 @@ import { ExpirationPlugin } from 'workbox-expiration'
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
 import { StaleWhileRevalidate } from 'workbox-strategies'
-import { updateWorker } from './serviceWorkerRegistration'
 
 declare const self: ServiceWorkerGlobalScope
 const ignore = self.__WB_MANIFEST
