a21c43e93007ea63eb9833dd748fdef2fbdbc091
Cho Yeonwoo <wooisso@naver.com>
2021-01-27 17:20:56 +0900

    error on location

--- src/views/location.tsx
+++ src/views/location.tsx
@@ -6,7 +6,10 @@ const LocationScreen = () => {
   }, [])
   return (<div style={{ width: '100%', height: 600, display: 'flex' }}>
     <RenderAfterNavermapsLoaded
-      clientId='q85oebl9tv'>
+      clientId='q85oebl9tv'
+      error={<p>Error</p>}
+      loading={<p>Loading...</p>}
+    >
       <NaverMap
         id='maps-getting-started-controlled'
         style={{ width: '100%', height: 600, flex: 1, display: 'flex' }}
