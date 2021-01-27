5509610d9b2ab38ffce2541c5994ec1c4bfcb2af
Cho Yeonwoo <wooisso@naver.com>
2021-01-27 17:46:48 +0900

    add onMessage console of fcm

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,2 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js')
 
+const messaging = firebase.messaging()
 
+messaging.onMessage((payload) => {
+  console.log(payload)
+})
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(payload)
+  self.registration.showNotification(payload.notification.title, { body: payload.notification.body, icon: 'favicon-16x16.png'})
+})
