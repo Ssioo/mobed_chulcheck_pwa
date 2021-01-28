1e97b837b91cc92111da9e6267bb0f1476366be3
Cho Yeonwoo <wooisso@naver.com>
2021-01-28 09:31:31 +0900

    update firebase messaging on background

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,11 +1,19 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/8.2.3/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.2.3/firebase-messaging.js')
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXthCGTDqn-f5Nd0MY5L-Nnxg8g-f6XtQ',
+  authDomain: 'mobedchulcheck.firebaseapp.com',
+  projectId: 'mobedchulcheck',
+  storageBucket: 'mobedchulcheck.appspot.com',
+  messagingSenderId: '1078947672432',
+  appId: '1:1078947672432:web:10b67c4c4a19ae9f4adfb5',
+  measurementId: 'G-CERXPFDHTC'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 const messaging = firebase.messaging()
-
-messaging.onMessage((payload) => {
-  console.log(payload)
-})
 
 messaging.onBackgroundMessage((payload) => {
   console.log(payload)
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+import  'firebase/messaging'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCXthCGTDqn-f5Nd0MY5L-Nnxg8g-f6XtQ',
@@ -16,6 +18,12 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 firebase.analytics()
+
+const messaging = firebase.messaging()
+firebase.messaging().getToken({ vapidKey: 'BFuII-gSgT5PGZwFUktwc49VCUmQURyMGexOTzkOcdS3_rNPDgZ9PJIvvs-1FMCBfIx65CevzmZ2O1mduWlugYM' })
+messaging.onMessage((payload) => {
+  console.log(payload)
+})
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 
