bb441be256a2a06eea241cc8e5aa5ae125917da9
Cho Yeonwoo <wooisso@naver.com>
2021-01-28 13:34:03 +0900

    skipwaiting

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -74,7 +74,8 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              updateAlert()
+              updateAlert(registration)
+
 
               // Execute callback
               if (config && config.onUpdate) {
--- src/util.tsx
+++ src/util.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
-export const updateAlert = () => {
+export const updateAlert = (registration) => {
   alert('새로운 업데이트가 있습니다.\n다시 시작하면 적용됩니다.')
+  registration.update()
 }
